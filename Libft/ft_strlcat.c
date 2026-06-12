b67eaf49ebe61b9ebd01d4b7479de2fbc384526e
vulcano42 <bthesix@gmail.com>
2026-06-12 11:02:57 -0300

    strlcat done

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -1,57 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thialima <thialima@student.42.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 13:59:42 by thialima          #+#    #+#             */
-/*   Updated: 2026/06/11 00:02:05 by thialima         ###   ########.fr       */
+/*   Created: 2026/06/10 23:55:12 by thialima          #+#    #+#             */
+/*   Updated: 2026/06/12 01:42:02 by thialima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*#include <libft.h>*/
 #include <stdio.h>
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 
 {
 	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 
+	dst_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
 	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
-
-{
-	char		*d;
-	const char	*s;
-	size_t		i;
-
-	i = 0;
-	d = dest;
-	s = src;
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	while (i < dstsize -1 && s[i])
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	while (src[i] && dst_len + i < dstsize - 1)
 	{
-		d[i] = s[i];
+		dest[dst_len + i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (ft_strlen(src));
+	dest[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
 /*
 int main (void)
 
 {
-	char dest[10];
-	 char	*src = "cinco";
+	char str[6] = "cinco";
+	char dest[5] = "seis";
 
-	
-	printf("%d",ft_strlcpy(dest,src,5));
+	printf ("%zu",ft_strlcat(dest,str,9));
+	return (0);
 }
 */

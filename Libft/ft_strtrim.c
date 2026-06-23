30db768c0f98c010dd26b208244bd22769f2b611
vulcano42 <bthesix@gmail.com>
2026-06-23 16:56:16 -0300

    atualização

---
+++ Libft/ft_strtrim.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thialima <thialima@student.42.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 03:31:47 by thialima          #+#    #+#             */
+/*   Updated: 2026/06/23 03:58:30 by thialima         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Libft.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+
+{
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen (s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end -1]))
+		end--;
+	return (ft_substr(s1, start, end - start));
+}
